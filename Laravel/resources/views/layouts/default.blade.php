a239fb8f53c395f0fcc0ab6834848464d51be311
xiezehao <798351803@qq.com>
2020-10-02 14:09:35 +0800

    切割头部和尾部子视图

--- Laravel/resources/views/layouts/default.blade.php
+++ Laravel/resources/views/layouts/default.blade.php
@@ -2,8 +2,17 @@
 <html>
 <head>
     <title>@yield('title','Weibo App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+<div class="container">
+    <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+</div>
+
 </body>
 </html>
