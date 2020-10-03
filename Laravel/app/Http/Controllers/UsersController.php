bba7d93ba292fd73624dc997b32effd58ac30bb9
xiezehao <798351803@qq.com>
2020-10-03 15:49:20 +0800

    用户显示页面
    用户显示页面

--- Laravel/app/Http/Controllers/UsersController.php
+++ Laravel/app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models;
 
 class UsersController extends Controller
 {
@@ -10,4 +12,10 @@ class UsersController extends Controller
     public function create(){
         return view("users.create");
     }
+
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+
+
 }
