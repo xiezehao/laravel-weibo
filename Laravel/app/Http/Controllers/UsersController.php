e66ee7d8f94a994d75981c8d80c83bedf93bc36a
xiezehao <798351803@qq.com>
2020-10-04 13:45:44 +0800

    访问策略
    访问策略

--- Laravel/app/Http/Controllers/UsersController.php
+++ Laravel/app/Http/Controllers/UsersController.php
@@ -9,6 +9,18 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'=>['show','create','store']
+        ]);
+
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
     //
     public function create(){
         return view("users.create");
@@ -37,10 +49,12 @@ class UsersController extends Controller
     }
 
     public function edit(User $user){
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user,Request $request){
+        $this->authorize('update', $user);
         $this->validate($request,[
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
@@ -53,7 +67,7 @@ class UsersController extends Controller
         }
         $user->update($data);
         session()->flash("success","个人资料更新成功！");
-        return redirect()->route("users.show",$user);
+        return redirect()->route("users.show",$user->id);
     }
 
 
--- Laravel/app/Http/Middleware/RedirectIfAuthenticated.php
+++ Laravel/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,9 +19,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+//            return redirect(RouteServiceProvider::HOME);
+            session()->flash("info","您已登录，无需再次操作。");
+            return redirect('/');
         }
 
-        return $next($request);
+//        return $next($request);
     }
 }
