50e621dc0d14961e19adc00892189cd45830f01e
xiezehao <798351803@qq.com>
2020-10-04 14:47:53 +0800

    查看用户列表
    查看用户列表

--- Laravel/app/Http/Controllers/UsersController.php
+++ Laravel/app/Http/Controllers/UsersController.php
@@ -13,7 +13,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except'=>['show','create','store']
+            'except'=>['show','create','store','index']
         ]);
 
         $this->middleware('guest',[
@@ -71,4 +71,8 @@ class UsersController extends Controller
     }
 
 
+    public function index(){
+        $users=User::paginate(10);
+        return view('users.index',compact('users'));
+    }
 }
