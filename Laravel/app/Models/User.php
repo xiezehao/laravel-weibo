dea2e3f67add953d78a2b0128bd256dc29de1619
xiezehao <798351803@qq.com>
2020-10-07 23:57:20 +0800

    11

--- Laravel/app/Models/User.php
+++ Laravel/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -58,7 +59,10 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
 
@@ -87,4 +91,6 @@ class User extends Authenticatable
     public function isFollowing($user_id){
         return $this->followings->contains($user_id);
     }
+
+
 }
